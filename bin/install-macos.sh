165c68e5e4519800534bd00ae27002733c337ade
Scott Miller <scott@millergeek.xyz>
2020-06-03 15:07:12 -0400

    add commit hooks

---
+++ bin/install-macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'installing brew packages'
+brew tap liamg/tfsec
+brew install tfenv tflint terraform-docs pre-commit liamg/tfsec/tfsec
+
+echo 'installing pre-commit hooks'
+pre-commit install
+
+echo 'setting pre-commit hooks to auto-install on clone in the future'
+git config --global init.templateDir ~/.git-template
+pre-commit init-templatedir ~/.git-template
+
+echo 'installing terraform with tfenv'
+tfenv install
