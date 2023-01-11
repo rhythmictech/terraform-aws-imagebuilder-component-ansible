e85204954d7b8d2968f502d2e09d0a4a5d83ecdf
David Goodell <dgoodell@rhythmictech.com>
2023-01-11 16:00:07 -0500

    Refactor to use API instead of Cloudformation

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.28"
+  required_version = ">= 0.14"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws",
-      version = ">= 2.44, < 4.0.0"
+      version = ">= 4.22.0"
     }
   }
 }
